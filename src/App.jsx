60a62407425fd416d4807cff4f8b095513c505d5
pedro0527 <sooyong0527@naver.com>
2025-05-12 21:46:33 +0900

    Feat : 실시간 시간 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import styled from "styled-components";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const nowTime = () => {
+    let now = new Date();
+    let hour = String(now.getHours()).padStart(2, "0");
+    let minute = String(now.getMinutes()).padStart(2, "0");
+    let second = String(now.getSeconds()).padStart(2, "0");
+
+    return `${hour} : ${minute} : ${second}`;
+  };
+  const [clock, setclock] = useState(nowTime);
+  setInterval(() => setclock(nowTime), 1000);
+
+  return (
+    <Wrapper>
+      <Clock>{clock}</Clock>
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.section`
+  display: flex;
+  width: 88vw;
+  height: 88vh;
+`;
+const Clock = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  font-size: 25px;
+`;
